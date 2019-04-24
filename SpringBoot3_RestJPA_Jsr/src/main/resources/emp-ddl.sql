3c63e814e20586fd268e6b410410230f9aff4d0d
kumargy <kumargy@gmail.com>
2019-04-24 17:26:40 +0530

    adding spring

---
+++ SpringBoot3_RestJPA_Jsr/src/main/resources/emp-ddl.sql
@@ -0,0 +1,10 @@
+--DROP TABLE EMPLOYEE_NG;
+CREATE Table EMPLOYEE_NG(
+EMP_ID number(6) primary key,
+FIRST_NAME varchar2(30),
+LAST_NAME varchar2(30),
+email varchar2(30),
+age number(3),
+salary number
+);
+create sequence emp_ng_seq start with 10 increment by 1;
